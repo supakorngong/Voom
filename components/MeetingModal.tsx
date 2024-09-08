be23604601871372f3829363e7b6ec3ad2967b0c
supakorngong <supurkorn@hotmail.com>
2024-09-08 16:47:51 +0700

    modal

---
+++ components/MeetingModal.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  buttonText?: string;
+  handleClick?: () => void;
+  className?: string;
+  children?: ReactNode;
+  image?: string;
+  buttonIcon?: string;
+}
+
+const MeetingModal = ({ isOpen, onClose, title, buttonText, handleClick, className, image, buttonIcon, children }: MeetingModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6 ">
+          {image && (
+            <div>
+              <Image src={image} width={72} height={72} alt="image" />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[32px]", className)}>{title}</h1>
+          <Button className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0" onClick={handleClick}>
+            {buttonIcon && <Image src={buttonIcon} width={13} height={13} alt="buttonIcon" />}&nbsp;
+            {buttonText && "Schedule Meeting"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MeetingModal;
